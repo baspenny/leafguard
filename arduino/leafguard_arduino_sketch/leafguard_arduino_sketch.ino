375cbe3fc330406e21e31432b6430e9238631371
DESKTOP-HUUB\Huub <huubverdel@gmail.com>
2017-12-02 12:25:56 +0100

    Arduino sketch aanpassing, getMoisturePercentage methode aangepast.

--- arduino/leafguard_arduino_sketch/leafguard_arduino_sketch.ino
+++ arduino/leafguard_arduino_sketch/leafguard_arduino_sketch.ino
@@ -1,10 +1,10 @@
 int sensorPin           = A3;
 int greenLed            = 11;
 int yellowLed           = 10;
-int redLed              = 9; 
+int redLed              = 9;
 int relaisSwitch        = 3;
 int moistureValue       = 0;
-int moisturePercentage  = 0;
+
 String readString;
 
 void setup() {
@@ -20,21 +20,21 @@ void setup() {
 void loop() {
 
   while (Serial.available()) {
-    delay(3);  
+    delay(3);
     char c = Serial.read();
-    readString += c; 
+    readString += c;
   }
   readString.trim();
-  if (readString.length() >0) {
+  if (readString.length() > 0) {
     if (readString == "pumpon")
     {
       digitalWrite(relaisSwitch, HIGH);
       digitalWrite(greenLed, HIGH);
       digitalWrite(redLed, LOW);
-      
+
       Serial.println("switching on");
     }
-    
+
     if (readString == "pumpoff")
     {
       digitalWrite(relaisSwitch, LOW);
@@ -43,30 +43,37 @@ void loop() {
 
       Serial.println("switching off");
     }
-    
-    if(readString == "data") 
+
+    if (readString == "data")
     {
+      moistureValue = analogRead(sensorPin);
+      moistureValue = map(moistureValue, 686, 1, 0, 100);
       String ret = "moisture=";
-      ret.concat(moisturePercentage);
+      ret.concat(getMoisturePercentage());
       ret.concat("&pumpstate=");
       ret.concat(digitalRead(relaisSwitch));
-      
+
       Serial.println(ret);
     }
-      
-
-    readString="";
-  } 
+    readString = "";
+  }
 }
-
-
 int getMoisturePercentage() {
-  return convertToPercent(analogRead(sensorPin));
+  moistureValue = analogRead(sensorPin);
+  moistureValue = map(moistureValue, 686, 1, 0, 100);
+  return moistureValue;
 }
 
-int convertToPercent(int value) {
-  return map(value, 686, 1, 0, 100);
-}
+
+
+
+
+
+
+
+
+
+
 
 
 
