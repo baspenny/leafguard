7f99a566d16723696aa72569bbe9ea81c5f79f00
AD\WV86XA <huubverdel@gmail.com>
2017-12-13 14:59:22 +0100

    ArduinoTest controlPump() draft

--- src/com/leafguard/tests/com/leafguard/leafguard/ArduinoTest.java
+++ src/com/leafguard/tests/com/leafguard/leafguard/ArduinoTest.java
@@ -7,17 +7,57 @@ import static org.junit.Assert.*;
 
 public class ArduinoTest {
 
-    private int id;
-    private int pumpState;
-    private int moisturePercentage;
-    private SerialConnectorTest serialConnector = new SerialConnectorTest();
+
+    private SerialConnector serialConnector;
 
 
+    public ArduinoTest(SerialConnector serialConnector) {
+        this.serialConnector = serialConnector;
+        serialConnector.initialize();
+    }
 
 
+    // deze test is nog niet af, nog testen met Arduino!
+    @Test
+    public void controlPump() throws Exception {
 
-    public void getDataFromSerial() {
-        this.parseReturnValue(this.serialConnector.receiveData());
+        int pumpState = 1; // input
+        Arduino arduino = new Arduino(serialConnector);
+
+        String expect = "Pomp staat al aan!";
+        String expect2 = "Pomp aan!"; // verwachting, nog controleren
+        String actual = arduino.controlPump(pumpState);
+
+        assertEquals(expect, expect2, actual);
+    }
+
+
+    @Test
+    public void getDataFromSerial1() throws Exception {
+    }
+
+    @Test
+    public void closeSerialConnection() throws Exception {
+    }
+
+    @Test
+    public void sleep() throws Exception {
+    }
+
+    @Test
+    public void sendData() throws Exception {
+    }
+
+    @Test
+    public void receiveData() throws Exception {
+    }
+
+    @Test
+    public void openConnection() throws Exception {
+    }
+
+    @Test
+    public void closeConnection() throws Exception {
     }
 
 
